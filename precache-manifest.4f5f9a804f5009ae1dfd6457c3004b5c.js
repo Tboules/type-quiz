1cdeba248bab41c8abe93888ecb74a3fff6e3e92
Tony Boules <tboules@gmail.com>
2020-10-06 09:29:27 -0700

    Updates

---
+++ precache-manifest.4f5f9a804f5009ae1dfd6457c3004b5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45189ec1fee03c9ad4427f088d8c9525",
+    "url": "/type-quiz/index.html"
+  },
+  {
+    "revision": "937afd5c1e919006f451",
+    "url": "/type-quiz/static/js/2.31a067dc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/type-quiz/static/js/2.31a067dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4264ca95009f15e0bc5",
+    "url": "/type-quiz/static/js/main.2e692bf6.chunk.js"
+  },
+  {
+    "revision": "9ee4101c565c4f30d6c2",
+    "url": "/type-quiz/static/js/runtime-main.09fbe78f.js"
+  },
+  {
+    "revision": "0db57796de3cfe68a3a7a4820c4486f6",
+    "url": "/type-quiz/static/media/drew-coffman.0db57796.jpg"
+  }
+]);
